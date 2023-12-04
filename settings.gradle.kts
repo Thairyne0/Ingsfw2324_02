5f6220d03247c99d21692ab68ae142b2b636c680
Thairyne0 <to.cesaro@studenti.unina.it>
2023-12-04 17:19:08 +0100

    Commit di prova

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ProgettoIngneria2324_02"
+include(":app")
+ 
