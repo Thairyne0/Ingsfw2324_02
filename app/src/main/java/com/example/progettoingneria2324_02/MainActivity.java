4fe12df42e955e50d7e940aa8c812b63675e3ea1
Thairyne0 <to.cesaro@studenti.unina.it>
2023-12-05 09:46:09 +0100

    Commit di prova di Tommaso

--- app/src/main/java/com/example/progettoingneria2324_02/MainActivity.java
+++ app/src/main/java/com/example/progettoingneria2324_02/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 int quevivaelporno = 5;
+int variabileTommaso = 3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
