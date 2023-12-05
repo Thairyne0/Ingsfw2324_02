decd0d48b0d4c95847ad7b64c33b8fc333837b62
Sh3g0 <claudiacoppola2002@gmail.com>
2023-12-05 09:50:10 +0100

    varabilenuova

--- app/src/main/java/com/example/progettoingneria2324_02/MainActivity.java
+++ app/src/main/java/com/example/progettoingneria2324_02/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 int quevivaelporno = 5;
 int variabileTommaso = 3;
+int varCladia=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
